6ad2fcb279cd0feb7a4d14659acd474c2ff45c70
Krasimir Genchev <krasimirhg@gmail.com>
2020-08-28 01:06:19 +0300

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI:
+		"mongodb+srv://Krasio:Krasimir@cluster0.fiyof.mongodb.net/bitak_db?retryWrites=true&w=majority",
+};
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+//body-parser middleware
+app.use(express.json());
+
+//db config
+const db = config.get("mongoURI");
+
+//connect to mongo
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+	})
+	.then(() => console.log("connect to mongo"))
+	.catch((err) => console.log(err));
+
+//use routes
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+//Serve static asserts if inproduction
+if (process.env.NODE_ENV === "production") {
+	//Set stattic folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server lissten on port ${port}`));
