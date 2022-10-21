6a1b650cc820796799b3c9b81ba9d8179b5f3453
Krasimir <krasimir.genchev@softwaregroup.com>
2022-10-21 19:06:27 +0300

    laptop

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,3 @@
 module.exports = {
-	mongoURI:
-		// "mongodb+srv://Krasio:Krasimir@cluster0.0lls6.mongodb.net/bitak_db?retryWrites=true&w=majority",
-		"mongodb://localhost:27017/localBitak"
+    mongoURI: 'mongodb://localhost:27017/localBitak'
 };
