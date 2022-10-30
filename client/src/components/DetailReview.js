f87287079768a934110c759a818eb0c5ce1db408
Krasio <krasimirhg@gmail.com>
2022-10-30 19:28:57 +0200

    image galery

--- client/src/components/DetailReview.js
+++ client/src/components/DetailReview.js
@@ -4,6 +4,7 @@ import Carousel from 'react-material-ui-carousel';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Image from "material-ui-image";
+import ImageGallery from 'react-image-gallery';
 
 import {deleteItem} from '../actions/itemActions';
 
@@ -26,37 +27,23 @@ class DetailReview extends Component {
     constructor(){
         super();
     }
-
-    // pics = this.props.item.filename;
-    // picName = this.props.item.name;
     
     render() {
-        let {filename: pics, name: picName, description} = this.props.item;
+        let {filename: pics, name, description} = this.props.item;
         if(!pics){
-           return <h2>Please, select a product</h2>
-        }
+            return <h2>Please, select a product</h2>
+         }
 
-        return (
-            <div style={{maxWidth: '70vw', maxHeight: '60vh'}}>
-            <Carousel 
-            indicatorIconButtonProps={{
-                style: {
-                    padding: '10px',    // 1
-                    color: 'blue'       // 3
-                }
-            }}
-            indicatorContainerProps={{
-                style: {
-                    marginTop: '50px', // 5
-                    textAlign: 'right' // 4
-                }
+           const images = this.props.item.filename.map(img => ({original: img,
+                thumbnail: img, originalWidth: '600px', thumbnailWidth: '150px'}));
         
-            }}
-            >
-            {
-                pics.map( (pic, i) => <Item key={picName} pic={pic} description={description} /> )
-            }
-        </Carousel>
+        return (
+            <div>
+        <h2>{name}</h2>
+        <ImageGallery
+           items={images}
+           />;
+        <p>{description}</p>
         </div>
         )
     }
