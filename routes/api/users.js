8cba7c88ca8fb446acbbe62ee4875d6cbd96d3ae
Krasio <krasimirhg@gmail.com>
2022-11-19 11:56:10 +0200

    add OTP and email service

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,9 @@ const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
 
+const { generateOTP } = require('../../services/otp');
+const { sendMail } = require('../../services/mailService');
+
 // User model
 const User = require('../../models/User');
 
@@ -17,44 +20,55 @@ router.post('/', (req, res) => {
     if (!firstName || !lastName || !email || !password) {
         return res.status(400).json({ msg: 'Please enter all fields' });
     }
+        // Check for existing user
+        User.findOne({ email }).then((user) => {
+            if (user) return res.status(400).json({ msg: 'User already exist' });
 
-    // Check for existing user
-    User.findOne({ email }).then((user) => {
-        if (user) return res.status(400).json({ msg: 'User already exist' });
+            // generate and store OTP
+            const otp = generateOTP();
+            // send OTP via email
+            sendMail({ to: email, otp }).then(info => {
+                if (!info) return res.status(400).json({ msg: 'Can not send OTP' });
+            })
 
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            password
-        });
-        // create salt and hash
+            const newUser = new User({
+                firstName,
+                lastName,
+                email,
+                password,
+                otp
+            });
 
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if (err) throw err;
-                newUser.password = hash;
-                newUser.save().then((user) => {
-                    jwt.sign(
-                        { id: user.id },
-                        config.get('jwtSecret'),
-                        { expiresIn: 3600 },
-                        (err, token) => {
-                            if (err) throw err;
-                            res.json({
-                                token,
-                                user: {
-                                    id: user.id,
-                                    firstName: user.firstName,
-                                    lastName: user.lastName,
-                                    email: user.email
-                                }
-                            });
-                        }
-                    );
+
+            // create salt and hash
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save().then((user) => {
+                        jwt.sign(
+                            { id: user.id },
+                            config.get('jwtSecret'),
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                if (err) throw err;
+                                res.json({
+                                    token,
+                                    user: {
+                                        id: user.id,
+                                        firstName: user.firstName,
+                                        lastName: user.lastName,
+                                        email: user.email,
+                                        isActive
+                                    }
+                                });
+                            }
+                        );
+                    });
                 });
             });
-        });
-    });
-});
+        })
+        .catch(err => res.status(400).json({ msg: err }))
+})
+
 module.exports = router;
